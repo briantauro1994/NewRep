b6e2299fd64b94c4c7633bbd489e18033f4da7f8
Brian Tauro <briantauro7@gmail.com>
2016-08-13 15:23:04 +0530

    Conflict 1

--- test.java
+++ test.java
@@ -2,8 +2,13 @@ package com;
 import java.util.*;
 public class test
 {
+public void conflict1()
+{
+System.out.println("This is Conflict 1");
+}
 public static void main(String[] args)
 {
 System.out.println("Welcome to Java Merged feature  1");
+conflict1();
 }
 }
