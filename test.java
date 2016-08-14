4ef0319f79b18c3915eb9dc4efa69f65043acfa9
briantauro7 <briantauro7@brian>
2016-08-14 05:55:22 +0530

    Test2

--- test.java
+++ test.java
@@ -12,13 +12,20 @@ System.out.println("This is Conflict 1");
 
 }
 
+public void conflict1()
+{
+
+System.out.println("This is Conflict 1");
+
+}
+
 public static void main(String[] args)
 {
 
 System.out.println("Welcome to Java Conflict  2");
 
 
-
+conflict1();
 conflict2();
 }
 }
