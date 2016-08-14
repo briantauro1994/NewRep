cc33acbe4dc611883979bee82b43783cada8b0b9
briantauro7 <briantauro7@brian>
2016-08-14 06:42:57 +0530

    1

--- test.java
+++ test.java
@@ -5,41 +5,6 @@ public class test
 {
 
 
-public void conflict4()
-{
-
-
-System.out.println("This is Never Conflict 2");
-
-System.out.println("This is feature 2");
-}
-
-public void conflict5()
-{
-
-
-System.out.println("This is Never Conflict 2");
-
-System.out.println("This is feature 2");
-}
-public void conflict2()
-{
-
-System.out.println("This is Conflict 2");
-
-}
-public void conflict5()
-{
-
-System.out.println("This is Conflict 2");
-
-}
-public void conflict3()
-{
-
-System.out.println("This is Conflict 3");
-
-}
 
 public void conflict1()
 {
@@ -55,7 +20,6 @@ System.out.println("Welcome to Java Conflict  2");
 
 
 conflict1();
-conflict2();
-conflict4();
+
 }
 }
